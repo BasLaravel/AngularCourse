9f7533d6a54dab74aa98a705bf2cb52f58392139
BasLaravel <38691103+BasLaravel@users.noreply.github.com>
2018-09-24 20:07:15 +0200

    laatste commit

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout( () => {
@@ -15,10 +19,17 @@ export class ServersComponent implements OnInit {
     }, 2000);
   }
 
-
-   
-
   ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName = event.target.value;
   }
 
 }
